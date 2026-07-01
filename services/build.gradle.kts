fb0a82a83cc2902aed0e4e76bbbe1cff7d082110
Lxiaoyao077 <barkerbigh7@gmail.com>
2026-07-01 09:32:56 +0800

    merge: combine manager-service + daemon-service into services

---
+++ services/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins { alias(libs.plugins.agp.lib) }
+
+android {
+    buildFeatures { aidl = true }
+
+    buildTypes { release { isMinifyEnabled = false } }
+
+    sourceSets {
+        named("main") {
+            java.srcDirs(
+                "manager-service/src/main/java",
+                "daemon-service/src/main/java",
+                "libxposed/service/src/main"
+            )
+            aidl.srcDirs(
+                "manager-service/src/main/aidl",
+                "daemon-service/src/main/aidl",
+                "libxposed/interface/src/main/aidl"
+            )
+        }
+    }
+
+    aidlPackagedList += "org/lsposed/lspd/models/Module.aidl"
+    namespace = "org.lsposed.lspd.services"
+}
+
+dependencies {
+    api(libs.rikkax.parcelablelist)
+    compileOnly(libs.androidx.annotation)
+    compileOnly(projects.shared.libxposedAnnotation)
+    compileOnly(projects.hiddenapi.stubs)
+}
